59f90f4532258581d497e714a5b2496012703f0e
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-08 17:20:32 +0200

    changed to print data to console

--- testPython/testPython.py
+++ testPython/testPython.py
@@ -3,12 +3,12 @@ import Adafruit_ADS1x15
 adc = Adafruit_ADS1x15.ADS1015()
 root = Tk()
 root.geometry("800x480")
-T = Text(root,height = 5, width = 5)
 
 
-for x in range(0,500): 
+
+for x in range(0,10): 
     data = adc.read_adc(1,gain=GAIN)
-    T.insert(tk.END, data)
+    print(data)
 
 
 root.mainloop()
