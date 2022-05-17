e3d48360a85a2f7be7fa980192ed6518f66b997a
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-17 15:38:27 +0200

    added temperature sensor reading

--- testPython/testPython.py
+++ testPython/testPython.py
@@ -1,45 +1,53 @@
-from tkinter import *
+#from tkinter import *
+
+#Regarding sensors from ADC:
 import time
 import Adafruit_ADS1x15
-adcport1 = Adafruit_ADS1x15.ADS1015(address=0x48)
-adcport2 = Adafruit_ADS1x15.ADS1015(address=0x49)
+adcforce = Adafruit_ADS1x15.ADS1015(address=0x48)
+adctemplight = Adafruit_ADS1x15.ADS1015(address=0x49)
 
+#Regarding temp sensor:
+import os
+import glob
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
-root = Tk()
-#root.geometry("800x480")
-#root.title('PythonTestProgram')
-
-#temperatur felt
-#frame = LabelFrame(root,padx=5,pady=5,bg="green",)
-#frame.pack(padx=10,pady=10)
-#frame.place(x=10,y=10)
-#Label(frame,text="TEMPERATUR").pack(pady=20)
-#lysfelt 
-#frame = LabelFrame(root,padx=5,pady=5,bg="green",)
-#frame.pack(padx=10,pady=10)
-#frame.place(x=10,y=100)
-#Label(frame,text="LYS").pack(pady=20)
-
-
-
-for x in range(0,100): 
-    data1 = adcport1.read_adc(1,gain=2/3)
-    data2 = adcport2.read_adc(2,gain=2/3)
-    print("data læst fra adc 1:" + data1)
-    print("data læst fra adc 2:" + data1)
-    time.sleep(4)
-
-
-#class ForceControl:
-    #def __init__(self, name):
-        #self.name = name
-
-    #def _update_widgets_(self,forceinput):
-
+#Readtemp raw method
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+#readtempincelcius
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+	
+while True:
+	print("Temperatur: " + str(read_temp()))	
+        data1 = adcforce.read_adc(1,gain=2/3)
+        data2 = adctemplight.read_adc(2,gain=2/3)
+        print("data læst fra adc 1:" + data1)
+        print("data læst fra adc 2:" + data2)
+	time.sleep(3)
 
 
 root.mainloop()
 
+
+ 
+ 
 
 
 
